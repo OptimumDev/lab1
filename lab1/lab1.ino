a16c8e89d1fd356e65a1b570e9391ca48dd55ca0
Artemiy <art.izakov@gmail.com>
2019-10-24 14:34:29 +0500

    finish task

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,28 +1,37 @@
-#include "pitches.h"
-#include "button.h"
-#include "buzzer.h"
+#include "rgb_led.h"
 
-#define PIN_BUZZER 6
-#define PIN_BUTTON_OFF 5
+#define FIRST_LED_PIN 1
+#define LEDS_COUNT 6
 
-Button buttonOff(PIN_BUTTON_OFF);
-Buzzer buzzer(PIN_BUZZER);
+void set_light_for_opposite_leds(int led_number, int light_power);
+void flash_lights(int current_number);
 
+const int LEDS_ARRAY_MIDDLE = LEDS_COUNT / 2;
 
-int notes[] = {NOTE_G3, NOTE_SILENCE, NOTE_G3, NOTE_SILENCE, NOTE_G3, NOTE_SILENCE, NOTE_DS3, NOTE_SILENCE};
-double durations[] = {8, 8, 1, 8, 1, 8, 1, 24};
-int melodyLength = 8;
+const rgb_led leds[LEDS_COUNT];
 
 void setup() {
-    buzzer.setMelody(notes, durations, melodyLength);
-    buzzer.turnSoundOn();
+  for (int i = 0; i < LEDS_COUNT; i++) {
+    int current_red_pin = FIRST_LED_PIN + i * 3;
+    leds[i] = rgb_led(current_red_pin, current_red_pin + 1, current_red_pin + 2);
+  }
 }
 
 void loop() {
+  for (int i = 0; i < LEDS_ARRAY_MIDDLE; i++) {
+    flash_lights(i);
+  }
+}
+
+void flash_lights(int current_number) {
+  for (int i = 0; i < 255; i++)
+    set_light_for_opposite_leds(current_number, i);
   
-    buzzer.playSound();
-    if (buttonOff.wasPressed())
-    {
-        buzzer.turnSoundOff();
-    }
+  for (int i = 255; i >= 0; i--)
+    set_light_for_opposite_leds(current_number, i);
+}
+
+void set_light_for_opposite_leds(int led_number, int light_power) {
+  leds[led_number].set_light(light_power);
+  leds[led_number + LEDS_ARRAY_MIDDLE].set_light(light_power);
 }
--- lab1/rgb_led.h
+++ lab1/rgb_led.h
@@ -2,16 +2,18 @@
 
 class rgb_led {
   public:
+    rgb_led() { }
+  
     rgb_led(int red_pin, int green_pin, int blue_pin) {
-      this.red_pin = red_pin;
-      this.green_pin = green_pin;
-      this.blue_pin = blue_pin;
+      red_pin = red_pin;
+      green_pin = green_pin;
+      blue_pin = blue_pin;
 
       pinMode(red_pin, OUTPUT);
       pinMode(green_pin, OUTPUT);
       pinMode(blue_pin, OUTPUT);
 
-      turn_off();
+      set_light(0);
     }
 
     void set_rgb(int r, int g, int b) {
@@ -20,22 +22,12 @@ class rgb_led {
       analogWrite(blue_pin, 255 - b);
     }
 
-    void turn_on() {
-      set_rgb(255, 255, 255);
-    }
-
-    void turn_off() {
-      set_rgb(0, 0, 0);
-    }
-
-    void flash(int duration) {
-      turn_on();
-      delay(duration);
-      turn_off();
+    void set_light(int power) {
+      set_rgb(power, power, power);
     }
 
   private:
     int red_pin;
     int green_pin;
     int blue_pin;
-}
+};
