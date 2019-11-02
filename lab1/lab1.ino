5f864735695187648d91f26895b392ddd581f227
Artemiy <art.izakov@gmail.com>
2019-11-02 17:45:50 +0500

    fix

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -3,8 +3,7 @@
 #define FIRST_LED_PIN 2
 #define LEDS_COUNT 6
 
-#define LIGHT_STEP_DELAY_TIME 15
-#define FLASHING_PAUSE_TIME 250
+#define LIGHT_STEP_DELAY_TIME 2
 
 void set_light_for_opposite_leds(int led_number, int light_power);
 void flash_lights(int current_number);
@@ -12,33 +11,43 @@ void flash_lights(int current_number);
 const int LEDS_ARRAY_MIDDLE = LEDS_COUNT / 2;
 
 const rgb_led leds[LEDS_COUNT];
+int current_powers[LEDS_COUNT];
 
 void setup() {
   for (int i = 0; i < LEDS_COUNT; i++) {
     leds[i] = rgb_led(FIRST_LED_PIN + i);
   }
+
+  for (int i = 0; i < LEDS_COUNT / 2; i++) {
+    int current_power = i * 320 / (LEDS_COUNT / 2);
+    current_powers[i] = current_power;
+    current_powers[i + LEDS_COUNT / 2] = current_power;
+  }
+
+  Serial.begin(115200);
 }
 
 void loop() {
-  for (int i = 0; i < LEDS_ARRAY_MIDDLE; i++) {
-    flash_lights(i);
-    delay(FLASHING_PAUSE_TIME);
-  }
+  update_lights();
+  update_powers();
+  delay(LIGHT_STEP_DELAY_TIME);
 }
 
-void flash_lights(int current_number) {
-  for (int i = 0; i < 255; i += 8) {
-    set_light_for_opposite_leds(current_number, i);
-    delay(LIGHT_STEP_DELAY_TIME);
+void update_lights() {
+  for (int i = 0; i < LEDS_COUNT; i++) {
+    int power = long(current_powers[i] - 160) * (current_powers[i] - 160) / 100;
+    power = power < 256 ? power : 255;
+    leds[i].set_light(power);
+    Serial.print(power);
+    Serial.print(" ");
   }
-  
-  for (int i = 255; i >= 0; i -= 8) {
-    set_light_for_opposite_leds(current_number, i);
-    delay(LIGHT_STEP_DELAY_TIME);
-  }
+  Serial.println("");
 }
 
-void set_light_for_opposite_leds(int led_number, int light_power) {
-  leds[led_number].set_light(light_power);
-  leds[(led_number + LEDS_ARRAY_MIDDLE) % LEDS_COUNT].set_light(light_power);
+void update_powers() {
+    for (int i = 0; i < LEDS_COUNT / 2; i++) {
+      int current_power = (current_powers[i] + 1) % 320;
+      current_powers[i] = current_power;
+      current_powers[i + LEDS_COUNT / 2] = current_power;
+  }
 }
